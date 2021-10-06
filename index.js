2b5c2bc30b8d99bb40495d554c634800c1e2c890
Vladimir Tishchenko <695amr@mail.ru>
2021-10-06 14:11:55 +0300

    new commit

--- index.js
+++ index.js
@@ -1,54 +1,33 @@
 const express = require('express');
 
-const app = express(); // Инициализируем express приложение
+const app = express();
 
-const LIMIT = process.env.LIMIT || 12000;
+const LIMIT = process.env.LIMIT || 8000;
 const DELAY = process.env.DELAY || 1000;
 const PORT = process.env.PORT || 3000;
 
-let connections = []; // Переменная в которой храним соединение
-
-function setConnection(resp) {
-    resp.setHeader('Content-type', 'text/html; charset=utf-8');
-    resp.setHeader('Transfer-Encoding', 'chunked'); // Таким образом указываем, что ответ передаем по кусочкам, а не сразу передаем весь ответ и закрываем соединение
-    connections.push(resp); // Сохраняем resp (объект при помощи которого мы сможем отправлять данные) в пуле соединений
-}
-
-function appLog() {
-    let num = 0
-    setTimeout(function run() {
-        num++
-        console.log(num, new Date());
-        setTimeout(run, DELAY);
-    }, DELAY)
-}
-
-function connectionsTimer() {
-    console.warn('Начали отсчет для всех соединений');
-    setTimeout(() => {
-        // let time = new Date();
-        connections.map((resp, i) => {
-            resp.write(`${i} client; ${new Date()}`); // Метод write не закрывает соединение, можно его исп чтоб отправлять данные кусочками, а render отправляем ответ и закрывает соед когда ответ полностью отправлен
-            resp.end();
-        })
-        process.exit();
-    }, LIMIT)
-}
+let allConnections = 0;
+let curConnections = 0;
 
 app.get('/date', (req, resp, next) => {
-    if (!connections.length) {
-        connectionsTimer()
-    }
-    setConnection(resp);
-    appLog();
+    let num = 0;
+    let apiNum = ++allConnections;
+    let curApiNum = curConnections++;
+
+    setTimeout(() => {
+        clearTimeout(timer);
+        curConnections--;
+        resp.send(new Date()+ '\n');
+    }, LIMIT);
+
+    let timer = setInterval(() => {
+        num++
+        console.log(' '.repeat(curApiNum), apiNum + '-е соединение:', num, new Date());
+    }, DELAY);
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
-})
-
-process.on('exit', () => {
-    console.warn('Закончили');
 })
 
 // Чтобы обратиться к этому серверу через консоль: "curl http://localhost:3000/date"
